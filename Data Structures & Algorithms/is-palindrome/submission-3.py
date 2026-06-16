e6ffd1716faf9aa85fe93abe541060a1a8789748
Austininc <136413167+Austininc@users.noreply.github.com>
2026-06-16 03:01:47 -0700

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,24 @@
+#test: Racecar,  , tab a cat, bob on the job
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:   
+        s_clean = [c for c in s.lower() if c.isalnum()]
+        return s_clean == s_clean[::-1]
+
+        # test
+        # s = Racecar
+        # s_clean = "racecar "
+        # racecar == racecar
+        
+        # tab a cat
+        # s = "tab a cat"
+        # s_clean = "tabacat "
+        # tabacat = tacabat
+        # True
+
+        #bob on the job
+        # s = "Bob on the job"
+        # s_clean = "bobonthejob"
+        # bobonthejob == bojehtnobob
+        #False 
